e30516cc9bf14f0fe6c0ae9ee9a14f3079064f5d
Andrew <andrew.casin@gmail.com>
2020-10-28 21:50:57 -0500

    commit tree 7aea9b6d4085ea78ceebfbb123f98f4eae4d9ef4

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,9 @@
 require 'pry'
 
 def my_find(collection)
-
+    i = 0
+    while i < collection.length
+        return collection[i] if yield(collection[i]) #statement modifier; Could also format "if, return"
+        i += 1
+    end
 end
